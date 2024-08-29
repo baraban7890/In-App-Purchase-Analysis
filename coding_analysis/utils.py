bc1fe70101e0e02f6a3b88b104b5d677387cba4f
baraban7890 <alexbaraban17@gmail.com>
2024-08-28 20:01:55 -0500

    Pushing more utils, made resampling into one method, changed standard_scaler to not scale y

--- coding_analysis/utils.py
+++ coding_analysis/utils.py
@@ -10,51 +10,17 @@ from imblearn.under_sampling import ClusterCentroids
 from imblearn.over_sampling import SMOTE
 from imblearn.combine import SMOTEENN
 
-#'Does smote and returns the dataframe'
-def do_smote(X,y,random_state=7):
-    smote = SMOTE(random_state=random_state)
-    X_smote, y_smote = smote.fit_resample(X, y)
-    smote_df = pd.DataFrame(X_smote, columns=X.columns)
-    smote_df[y.name] = y_smote
-    return smote_df
-
-#'Does smoteen and returns the dataframe'
-
-def do_smoteen(X,y,random_state=7):
-    smoteen = SMOTEENN(random_state=random_state)
-    X_smoteen, y_smoteen = smoteen.fit_resample(X, y)
-    smoteen_df = pd.DataFrame(X_smoteen, columns=X.columns)
-    smoteen_df[y.name] = y_smoteen
-    return smoteen_df
-
-#'Does random undersampler and returns the dataframe'
-def do_rus(X,y,random_state=7):
-    rus = RandomOverSampler(random_state=random_state)
-    X_rus, y_rus = rus.fit_resample(X,y)
-    rus_df = pd.DataFrame(X_rus,columns=X.columns)
-    rus_df[y.name] = y_rus
-    return rus_df
-
-#'Does random oversampler and returns the dataframe'
-def do_ros(X,y,random_state=7):
-    ros = RandomUnderSampler(random_state=random_state)
-    X_ros, y_ros = ros.fit_resample(X,y)
-    ros_df = pd.DataFrame(X_ros,columns=X.columns)
-    ros_df[y.name] = y_ros
-    return ros_df
-
-#'Does random cluster centroids and returns the dataframe'
-def do_cc(X,y,random_state=7):
-    cc = ClusterCentroids(random_state=random_state)
-    X_cc, y_cc = ros.fit_resample(X,y)
-    cc_df = pd.DataFrame(X_cc,columns=X.columns)
-    cc_df[y.name] = y_cc
-    return cc_df
+def apply_resampling(X, y, method, random_state=7):
+    sampler = method(random_state=random_state)
+    X_resampled, y_resampled = sampler.fit_resample(X, y)
+    resampled_df = pd.DataFrame(X_resampled, columns=X.columns)
+    resampled_df[y.name] = y_resampled
+    return resampled_df
 
 #'Does standard scaler and returns the dataframe'
-def standard_scale(X,y):
+def standard_scale(X):
     sc = StandardScaler()
-    scaled_data = sc.fit_transform(X,y)
+    scaled_data = sc.fit_transform(X)
     scaled_df = pd.DataFrame(scaled_data,columns=X.columns)
     return scaled_df
 
@@ -99,24 +65,23 @@ def preprocess_and_clean(gaming_df, smote=False, smoteen=False, rus = False, ros
         if smoteen or rus or ros or cc:
             raise ValueError("Only one of smote, smoteen, rus, ros, or cc can be True at the same time.")
         else:
-            gaming_df = do_smote(X,y)
+            gaming_df = apply_resampling(X,y,SMOTE)
     elif smoteen:
         if smote or rus or ros or cc:
             raise ValueError("Only one of smote, smoteen, rus, ros, or cc can be True at the same time.")
         else:
-            gaming_df = do_smoteen(X,y)
+            gaming_df = apply_resampling(X,y,SMOTEENN)
     elif rus:
         if smoteen or smote or ros or cc:
             raise ValueError("Only one of smote, smoteen, rus, ros, or cc can be True at the same time.")
         else:
-            gaming_df = do_rus(X,y)
+            gaming_df = apply_resampling(X,y,RandomUnderSampler)
     elif cc:
         if smoteen or rus or ros or smote:
             raise ValueError("Only one of smote, smoteen, rus, ros, or cc can be True at the same time.")
         else:
-            gaming_df = do_cc(X,y)
+            gaming_df = apply_resampling(X,y, cc)
     if pca: ### FIX THIS
         pca_df = do_pca(X,y)
-        gaming_df = standard_scale(pca_df,y)
-        return gaming_df
+        gaming_df = standard_scale(pca_df)
     return gaming_df
