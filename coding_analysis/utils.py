e35df38cf50276ca5b569495c72cec0b65febd14
baraban7890 <alexbaraban17@gmail.com>
2024-08-29 19:04:29 -0500

    finished polishing up utils.py, still some changes left though

--- coding_analysis/utils.py
+++ coding_analysis/utils.py
@@ -14,7 +14,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
+def split_data(df):
+    X = df.drop(columns = 'InGamePurchases')
+    y = df['InGamePurchases']
+    return X,y
+
 def apply_resampling(X, y, method, random_state=7):
+    '''
+        This method takes in features and target dataframes (or series for target)
+        and applies the sampling method passed in to 'method', for example SMOTE
+    '''
     sampler = method(random_state=random_state)
     X_resampled, y_resampled = sampler.fit_resample(X, y)
     resampled_df = pd.DataFrame(X_resampled, columns=X.columns)
@@ -23,12 +32,20 @@ def apply_resampling(X, y, method, random_state=7):
 
 #'Does standard scaler and returns the dataframe'
 def standard_scale(X):
+    '''
+        This method does standard scale (mean 0 variance 1) and returns the scaled dataframe
+        Should only be for feature dataframe (X)
+    '''
     sc = StandardScaler()
     scaled_data = sc.fit_transform(X)
     scaled_df = pd.DataFrame(scaled_data,columns=X.columns)
     return scaled_df
 
 def do_pca(X,y,num_components=7):
+    '''
+        This method does PCA with a specified number of components.
+        Returns a dataframe with the PCA weights, the model, and the weights themselves in that order.
+    '''
     pca = PCA(n_components=num_components)
     components = pca.fit_transform(X,y)
     component_range = range(1,num_components+1)
@@ -42,6 +59,12 @@ def do_pca(X,y,num_components=7):
 
 
 def preprocess_and_clean(gaming_df, smote=False, smoteenn=False, rus = False, ros = False, cc = False, pca = False):
+    '''
+        This method preprocesses the data by first ordinally encoding the Gamedifficulty and EngagementLevel columns.
+        Then it one hot encodes the columns Gender, Location, and GameGenre.
+        Then it splits it up into the features (X) and the target (y) which is InAppPurchases.
+        Depending on which sampler is "True", it will apply that to the dataframe.
+    '''
     custom_mapping = [
         ['Easy', 'Medium', 'Hard'],  # Custom order for 'GameDifficulty', 0 is easy, 1 is medium, 2 is hard
         ['Low', 'Medium', 'High']  # Custom order for 'EngagementLevel', 0 is low, 1 is medium, 2 is high
@@ -54,7 +77,6 @@ def preprocess_and_clean(gaming_df, smote=False, smoteenn=False, rus = False, ro
     gaming_df[['GameDifficulty','EngagementLevel']] = encodings
 
 
-
     ohe = OneHotEncoder(sparse_output=False,dtype='int')
 
     ohe_df = pd.DataFrame(data=ohe.fit_transform(gaming_df[['Gender','Location','GameGenre']]), columns=ohe.get_feature_names_out())
@@ -62,8 +84,8 @@ def preprocess_and_clean(gaming_df, smote=False, smoteenn=False, rus = False, ro
     gaming_df = pd.concat([gaming_df, ohe_df], axis=1)
     gaming_df = gaming_df.drop(columns = ['PlayerID','PlayTimeHours','GameGenre','Location','Gender'])
 
-    X = gaming_df.drop(columns = 'InGamePurchases')
-    y = gaming_df['InGamePurchases']
+
+    X,y = split_data(gaming_df)
 
     if smote:
         if smoteenn or rus or ros or cc:
@@ -90,21 +112,21 @@ def preprocess_and_clean(gaming_df, smote=False, smoteenn=False, rus = False, ro
             raise ValueError("Only one of smote, smoteen, rus, ros, or cc can be True at the same time.")
         else:
             gaming_df = apply_resampling(X,y, ClusterCentroids)
-    if pca: ### FIX THIS
+    if pca: ### TO DO
         pca_df = do_pca(X,y)
         gaming_df = standard_scale(pca_df)
     return gaming_df
 
 ### Creates and fits a model of your choosing
-def create_and_fit_model(model,X_train,y_train,num=10,random_state=7,max_iter=1000):
+def create_and_fit_model(model,X_train,y_train,num=10,random_state=7,max_iter=1000, kernel='rbf'):
     if model is RandomForestClassifier:
         pred_model = model(random_state=random_state,n_estimators=num)
     elif model is KNeighborsClassifier:
         pred_model = model(random_state=random_state,n_neighbors=num)
     elif model is LogisticRegression:
-        pred_model = model(random_state=random_state,max_iter=1000)
+        pred_model = model(random_state=random_state,max_iter=max_iter)
     else:
-        pred_model = model(random_state=random_state)
+        pred_model = model(random_state=random_state, kernel=kernel)
     pred_model.fit(X_train,y_train)
     return pred_model
 
@@ -113,3 +135,4 @@ def eval_model(fit_model,X_test,y_test):
     print(f'confusion matrix: {confusion_matrix(y_test,y_pred)}')
     print(f'balanced accuracy score: {balanced_accuracy_score(y_test,y_pred)}')
     print(f'classification report: {classification_report(y_test,y_pred)}')
+
